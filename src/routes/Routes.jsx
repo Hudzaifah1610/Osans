ca68a54780b4a66bf6ae446557db433bae78b097
Hudzaifah1610 <Hudzaifah1610@gmail.com>
2019-10-19 10:26:10 +0700

    first commit

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,15 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import LandingPage from "../components/LandingPage/LandingPage";
-import Register from "../container/Register/Register";
-import Login from "../container/Login/Login";
+import { BrowserRouter as Router } from "react-router-dom";
+import Chatting from "../container/Chatting/Chatting";
 
 function Routes() {
   return (
     <Router>
-      <Route exact path="/" component={LandingPage} />
-      <Route path="/register" component={Register} />
-      <Route path="/login" component={Login} />
+      <Chatting/>
     </Router>
   );
 }
